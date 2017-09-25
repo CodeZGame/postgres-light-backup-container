db9edf4bf04a79d0735e3e81ebc440b209b79b89
BenEko <benjamin.francoual@outlook.com>
2017-09-25 15:06:22 +0200

    first

---
+++ bin/job.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+DATE=$(date +%Y-%m-%d-%H-%M)
+export PGPASSWORD=$POSTGRESQL_PASSWORD
+pg_dump --username=$POSTGRESQL_USER --host=$POSTGRESQL_SERVICE_HOST --port=$POSTGRESQL_SERVICE_PORT $POSTGRESQL_DATABASE > /tmp/dump.sql
+
+if [ $? -ne 0 ]; then
+    echo "db-dump not successful: ${DATE}"
+    exit 1
+fi
+
+gzip -c /tmp/dump.sql > $BACKUP_DATA_DIR/dump-${DATE}.sql.gz
+
+if [ $? -eq 0 ]; then
+    echo "backup created: ${DATE}"
+else
+    echo "backup not successful: ${DATE}"
+    exit 1
+fi
+
+# Delete old files
+old_dumps=$(ls -1 $BACKUP_DATA_DIR/dump* | head -n -$BACKUP_KEEP)
+if [ "$old_dumps" ]; then
+    echo "Deleting: $old_dumps"
+    rm $old_dumps
+fi
+
+#Copy backup to EkoServ (rugby DB "proxy")
+scp -o UserKnownHostsFile=.eko_ssh/known_hosts -p -i .eko_ssh/id_rsa $BACKUP_DATA_DIR/dump-${DATE}.sql.gz $EKOSERVER_USER@$EKOSERVER_HOST:/home/ekouser/HTS2Backups/$DB_BACKUP_FILE
+echo "[INFO] Backup sent to Eko server"
+
+#Remove backup older than 8 days on EkoServer (careful, directory cannot use env value)
+ssh -i .eko_ssh/id_rsa $EKOSERVER_USER@$EKOSERVER_HOST -o UserKnownHostsFile=.eko_ssh/known_hosts 'find ~/HTS2Backups/ -maxdepth 1 -type f -name "*.backup*" -mtime +7 -exec rm {} \;'
+echo "[INFO] Clean old backups on Eko server"
+
+function cleanup {
+    rm -f /tmp/dump.sql.gz /tmp/dump.sql
+}
+
+trap cleanup EXIT
